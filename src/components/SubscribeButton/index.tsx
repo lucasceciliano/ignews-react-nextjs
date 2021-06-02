9a6e47a55c20d13b3660f92e898b16b1053e5fa7
Lucas Ceciliano <cecilianolucas@gmail.com>
2021-06-01 21:32:39 -0300

    ...

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,42 +1,45 @@
 import { signIn, useSession } from 'next-auth/client'
+import { useRouter } from 'next/dist/client/router'
 import { api } from '../../services/api'
-import styles from './styles.module.scss'
 import { getStripeJs } from '../../services/stripe-js'
+import styles from './styles.module.scss'
 
-interface SubscribeButtoProps {
-    priceId: string
+interface SubscribeButtonProps {
+  priceId: string;
 }
 
-export function SubscribeButton({priceId}: SubscribeButtoProps) {
-
-    const [session] = useSession()
-
-    async function handleSubscribe() {
-        if(!session) {
-        signIn('github')
-        return
-        }
-
-
-        try{
-            const response = await api.post('/subscribe')
-
-            const { sessionId } = response.data
-
-            const stripe = await getStripeJs()
-
-            await stripe.redirectToCheckout({sessionId})
-        } catch (err) {
-            alert(err.menssage);
-        }
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession()
+  const router = useRouter()
+  async function handleSubscribe() {
+    if(!session) {
+      signIn('github')
+      return
     }
 
-    return(
-        <button
-        type="button"
-        className={styles.subscribeButton}
-        >
-            Subscribe Now
-        </button>
-    )
+    if(session?.activeSubscription) {
+      router.push('/posts')
+      return
+    }
+
+    try {
+      const response = await api.post('subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}>
+      Subscribe now
+    </button>
+  )
 }
